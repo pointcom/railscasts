d12c159dee828f87169f9a26ac3e035f3a3e148d
Mathieu Fosse <mathieu.fosse@gmail.com>
2008-11-30 13:29:03 +0100

    Added Vimeo id

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080820014608) do
+ActiveRecord::Schema.define(:version => 20081130122433) do
 
   create_table "comments", :force => true do |t|
     t.integer  "episode_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(:version => 20080820014608) do
     t.datetime "updated_at"
     t.integer  "position",       :default => 0
     t.integer  "comments_count", :default => 0, :null => false
+    t.integer  "vimeo_id"
   end
 
   create_table "sponsors", :force => true do |t|
